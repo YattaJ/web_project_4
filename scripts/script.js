ed77bf985093b2164e4f24e941667f0cffc08635
YattaJ <86165464+YattaJ@users.noreply.github.com>
2021-11-07 08:50:54 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -93,7 +93,7 @@ function closeModal(modal) {
 }
 
 // Close card modal w/o saving
-function CloseBtnsHandler(evt) {
+function initializeCloseBtns(evt) {
   closeModal(evt.target.closest(".modal"));
 }
 
